c63f31020ed718c3877ae0cf18426bb027afb356
vamsi krishna <iamvkrishna24@gmail.com>
2025-07-20 01:02:28 +0530

    test file added to test POST API

--- Udyog/home/views.py
+++ Udyog/home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home/home.html')
+
+def test(request):
+    return render(request, 'home/test.html')
