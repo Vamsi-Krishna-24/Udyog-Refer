ab43369c46c6ffec364ac24cd2c30a5df698e95f
vamsi krishna <iamvkrishna24@gmail.com>
2025-07-26 12:36:33 +0530

    login&signup

--- Udyog/Udyog/settings.py
+++ Udyog/Udyog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,14 +74,7 @@ WSGI_APPLICATION = 'Udyog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'udyogrefer',              
-        'USER': 'surisettivamsikrishna',                   
-        'PASSWORD': 'Vamsi@2408', 
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 
