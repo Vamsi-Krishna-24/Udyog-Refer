66b3ca51eb448fa30e25b0b181071b07afce4ef7
vamsi krishna <iamvkrishna24@gmail.com>
2025-08-26 20:03:01 +0530

    Pagination addded

--- Udyog/Udyog/settings.py
+++ Udyog/Udyog/settings.py
@@ -56,10 +56,13 @@ MIDDLEWARE = [
 
 #ADDING REST FRAMEWORK -- JWT LOGIN
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,   # -----> 10 cards per page
 }
+
 
 
 
