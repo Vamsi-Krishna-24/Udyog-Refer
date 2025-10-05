9287b299176b1c945ae6acf857ef5d7f8027224f
vamsi krishna <iamvkrishna24@gmail.com>
2025-10-05 07:17:00 +0530

    Change in UI

--- Udyog/Udyog/settings.py
+++ Udyog/Udyog/settings.py
@@ -61,9 +61,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,   # -----> 10 cards per page
+    )
 }
 
 
