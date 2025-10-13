701280e44293dbec7884ce01408023d7adbe5f1e
vamsi krishna <iamvkrishna24@gmail.com>
2025-10-13 19:34:50 +0530

    redesigned REFERER Home

--- Udyog/home/views.py
+++ Udyog/home/views.py
@@ -19,6 +19,8 @@ from .serializers import JobSerializer
 from rest_framework import viewsets, filters, serializers
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
+from rest_framework.pagination import PageNumberPagination
+
 
 
 
