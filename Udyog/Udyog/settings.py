c0fdd390e331abb42c426589606c7432989cecf5
vamsi krishna <iamvkrishna24@gmail.com>
2025-12-02 17:37:13 +0530

    Added Prod DB

--- Udyog/Udyog/settings.py
+++ Udyog/Udyog/settings.py
@@ -104,8 +104,15 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_m2wpizOBe4tW',
+        'HOST': 'ep-orange-shape-ahninqg2-pooler.c-3.us-east-1.aws.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
 
